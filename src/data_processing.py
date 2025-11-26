5e235d771312e73b82e0907c34366f7de7e3cd12
apedden927 <aaronpedden088@gmail.com>
2025-11-26 16:24:51 -0500

    s

--- src/data_processing.py
+++ src/data_processing.py
@@ -6,4 +6,5 @@ def load_experiments():
     for i in range(1, 19):
         df = pd.read_csv(f"data/experiment_{i:02d}.csv")
         experiments.append(df)
-    return experiments
+    dftrain = pd.read_csv(f"data/train.csv")
+    return experiments, dftrain
